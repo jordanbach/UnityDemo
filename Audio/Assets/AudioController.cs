d50fc7603a4c5d2450d7b7a2602ebe14d9a4b52a
Jordan Bach <jordan@fablevision.com>
2021-04-14 21:00:08 -0400

    Initial commit of Unity Project

---
+++ Audio/Assets/AudioController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+
+    public AudioClip clip1;
+    public AudioClip clip2;
+    public AudioClip clip3;
+
+    public AudioSource mySource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayClip1()
+    {
+        mySource.clip = clip1;
+        mySource.Play();
+    }
+
+    public void PlayClip2()
+    {
+        mySource.clip = clip2;
+        mySource.Play();
+    }
+
+    public void PlayClip3()
+    {
+        mySource.clip = clip3;
+        mySource.Play();
+    }
+}
